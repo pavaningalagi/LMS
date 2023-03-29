3f60f4242bed5e725eb34e293bf09ad045b5bb2d
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-03-30 02:04:50 +0530

    login page with required

---
+++ Frontend/src/index.js
@@ -0,0 +1,14 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const login = document.getElementById("login");
+const form = document.getElementById("form");
+
+// form.addEventListener("submit",(event)=>{
+// event.preventDefault();
+
+// });
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+    // api req verification;
+};
