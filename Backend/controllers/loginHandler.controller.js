ce6f83e9e5535ba41e4bee0d84da234b9772d266
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-03-30 11:51:58 +0530

    frontend and backend

--- Backend/controllers/loginHandler.controller.js
+++ Backend/controllers/loginHandler.controller.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const {user_module} = require("../modules/user.module");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const { user_module } = require("../modules/user.module");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-
-const loginHandler = async(req,res) =>{
-    const data = req.body;
-    console.log(data);
-    try {
-        const saltRounds = 10;
-        const myPlaintextPassword = data.password;
-        const userData = await user_module.findOne({email:data.email} || {name:data.name});
-        if(userData){
-            const hash = userData.password;
-            bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-                if(result) {
-                // result == true
-                jwt.sign({ serID: userData._id }, "privateKey", function(err, token) {
-                    // console.log(token);
-                    // console.log(result);
-                res.send({"msg":"Welcome to Sapient training",
-            "token": token});
-                  });
-                }else{
-                    res.send({"msg":"Invalid credentials"});
-                }
-            });
-            
-        }else{
-            res.send("Please Sign up");
-        }
-        
-    } catch (error) {
-        res.send({"error": error.message});
+const loginHandler = async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  try {
+    const myPlaintextPassword = data.password;
+    let userData = "";
+    if (data.email) {
+      userData = await user_module.findOne({ email: data.email });
+    } else if (data.name) {
+      userData = await user_module.findOne({ name: data.name });
     }
-
+    if (userData) {
+      const hash = userData.password;
+      bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+        if (result) {
+          // result == true
+          jwt.sign(
+            { serID: userData._id },
+            "privateKey",
+            function (err, token) {
+              // console.log(token);
+              // console.log(result);
+              res.send({ msg: "Welcome to Sapient training", token: token });
+            }
+          );
+        } else {
+          res.send({ msg: "Invalid credentials" });
+        }
+      });
+    } else {
+      res.send("Please Sign up");
+    }
+  } catch (error) {
+    res.send({ error: error.message });
+  }
 };
 
-
-module.exports ={
-    loginHandler
-}
+module.exports = {
+  loginHandler,
+};
