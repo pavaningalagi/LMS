107ff8b624b4e1d456cffa2fd42642a6f78a070a
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-03-30 11:53:05 +0530

    final code

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,50 @@
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const ip_mailId = document.getElementById("email");
+const pass = document.getElementById("password");
 const login = document.getElementById("login");
 const form = document.getElementById("form");
+const url = "http://localhost:8080";
 
-// form.addEventListener("submit",(event)=>{
-// event.preventDefault();
 
-// });
-
-form.onsubmit = (e)=>{
-    e.preventDefault();
-    // api req verification;
-    if(form.email.value && form.password.value){
-        console.log(1);
+// On Click Login:
+form.onsubmit = async (e) => {
+  e.preventDefault();
+  // api req verification;
+  // console.log(1);
+  if (password.length <= 5) {
+    alert("Password must be at least 5 characters long.");
+    return false;
+  }
+  try {
+    let regExp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.[a-zA-Z]+$";
+    // let input;
+    if (form.email.value.match(regExp)) {
+      let res = await fetch(`${url}/users/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          email: form.email.value,
+          password: form.password.value,
+        }),
+        headers: { "content-type": "application/json" },
+      });
+      res = await res.json();
+      const token = res.token;
+      localStorage.setItem("token", token);
+      console.log(res);
+    } else {
+      let res = await fetch(`${url}/users/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          name: form.email.value,
+          password: form.password.value,
+        }),
+        headers: { "content-type": "application/json" },
+      });
+      res = await res.json();
+      const token = res.token;
+      localStorage.setItem("token", token);
+      console.log(res);
     }
+  } catch (error) {
+    console.log({ error: error.message });
+  }
 };
